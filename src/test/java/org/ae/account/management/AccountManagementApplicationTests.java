2ea3455ba70c326c25f39e081baaacd8f0553a9f
Abdelouahedd <ennouriabdelouahed@gmail.com>
2022-11-01 09:18:15 +0100

    feat: first commit

---
+++ src/test/java/org/ae/account/management/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ae.account.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
